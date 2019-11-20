24e80caf5d8949da1c3e6ef0af5902bfd4b09ce4
marinedzl <marinedzl01@gmail.com>
2019-11-20 14:24:47 +0800

    将SkeletalMeshMerge移出引擎

--- Source/TestAction/TestAction.Build.cs
+++ Source/TestAction/TestAction.Build.cs
@@ -8,7 +8,7 @@ public class TestAction : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RenderCore", "RHI" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
